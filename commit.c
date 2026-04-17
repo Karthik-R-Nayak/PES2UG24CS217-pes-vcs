ec21fb804cb8b925f35c82262fb61fa6fb46496f
Karthik Nayak <karthikrnayakhdk2006@gmail.com>
2026-04-17 23:22:25 +0530

    Update commit.c

--- commit.c
+++ commit.c
@@ -195,8 +195,7 @@ int head_update(const ObjectID *new_commit) {
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
     // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    int commit_create(const char *message, ObjectID *commit_id_out) {
+    // (See Lab Appendix for logical steps
     // 1. Build tree from index
     ObjectID tree_id;
     if (tree_from_index(&tree_id) != 0) return -1;
